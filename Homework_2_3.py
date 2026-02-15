0fb17de82683a4695f5b8660bb6e5f7310583eba
DmytroHryhorashenko <dimon09051991@gmail.com>
2026-02-15 11:23:53 +0200

    new

--- Homework_2_3.py
+++ Homework_2_3.py
@@ -1,8 +1,7 @@
 import sys
 from pathlib import Path
-from colorama import init, Fore, Style
+from colorama import init, Fore
 
-# ініціалізація colorama
 init(autoreset=True)
 
 
@@ -10,30 +9,26 @@ def print_directory_structure(path: Path, indent: str = ""):
     try:
         for item in sorted(path.iterdir()):
             if item.is_dir():
-                print(indent + Fore.BLUE + f"📂 {item.name}")
+                print(indent + Fore.BLUE + item.name + "/")
                 print_directory_structure(item, indent + "    ")
             else:
-                print(indent + Fore.GREEN + f"📜 {item.name}")
+                print(indent + Fore.GREEN + item.name)
     except PermissionError:
-        print(indent + Fore.RED + "⛔ Немає доступу")
+        print(indent + Fore.RED + "Немає доступу")
 
 
 def main():
     if len(sys.argv) != 2:
-        print(Fore.RED + "❗ Використання: python hw03.py <шлях_до_директорії>")
+        print(Fore.RED + "Використання: python hw03.py <шлях>")
         sys.exit(1)
 
     dir_path = Path(sys.argv[1])
 
-    if not dir_path.exists():
-        print(Fore.RED + "❗ Вказаний шлях не існує.")
+    if not dir_path.exists() or not dir_path.is_dir():
+        print(Fore.RED + "Некоректний шлях до директорії")
         sys.exit(1)
 
-    if not dir_path.is_dir():
-        print(Fore.RED + "❗ Вказаний шлях не є директорією.")
-        sys.exit(1)
-
-    print(Fore.CYAN + f"\n📦 {dir_path.name}")
+    print(Fore.CYAN + dir_path.name + "/")
     print_directory_structure(dir_path)
 
 
